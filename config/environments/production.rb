e1a85874cf859d647c188969d1e2aed668b8ad01
Anatolii Borysov <borisano@gmail.com>
2025-08-08 17:42:34 +0100

    Set up kamal deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: ENV.fetch("DEPLOYMENT_HOST", "alchefeast.borisano.com") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -80,11 +80,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    ENV.fetch("DEPLOYMENT_HOST", "alchefeast.borisano.com"),     # Allow requests from your domain
+    ENV.fetch("DEPLOYMENT_IP", "16.170.188.85")                # Allow requests from server IP
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
